f3a3e40aac68e328c1d1103bd140ef30f8ee9336
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-12 01:44:51 +0000

    implemented 2 functions: createTweetElement appends html structure w/ tweet object data to existing html container. renderTweet to take in array of tweets and calls createTweetElement on each tweet object passed in from array.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,33 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+$(document).ready(function(){
+
+function createTweetElement (tweet) {
+  var $tweet = $("<article class='tweet'>")
+    .append($("<header>")
+      .append($("<img class='avatar' src='" + tweet.user.avatars.small + "'>"))
+      .append($("<h3>").text(tweet.user.name))
+      .append($("<h4>").text(tweet.user.handle))
+    )
+    .append($("<p>").text(tweet.content.text))
+    .append($("<footer>")
+      .append($("<h5>").text(tweet.created_at))
+      .append($("<span class='icons'>")
+        .append($("<img class='save' src='/images/save.png'>"))
+        .append($("<img class='retweet' src='/images/retweet.png'>"))
+        .append($("<img class='like' src='/images/like.png'>"))
+        )
+    );
+  return $tweet;
+}
+
+function renderTweets (tweets){
+  tweets.forEach(function(tweetObj){
+    var tweet = createTweetElement(tweetObj);
+    $("#tweets-container").append(tweet);
+  });
+}
+
+});
+
+
 
