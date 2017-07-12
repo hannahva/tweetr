975ef5dde3e6ab86603a610f177b86bba8c91449
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-12 22:40:04 +0000

    added event listener so counter resets to 140 on submit event

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   $(".new-tweet textarea").on("keyup", function(){
-    var input = $(this).val();
-    var remaining = 140 - input.length;
-    var counter = $(this).closest("form").find(".counter");
-    counter.text(remaining);
+    var $input = $(this).val();
+    var remaining = 140 - $input.length;
+    var $counter = $(this).closest("form").find(".counter");
+    $counter.text(remaining);
     if (remaining < 0) {
-      counter.addClass("error");
+      $counter.addClass("error");
     } else {
-      counter.removeClass("error");
+      $counter.removeClass("error");
     }
   });
+  const $form = $('#create-tweet');
+  $form.on('submit', function(event){
+    $(".counter").text("140");
+  })
 });
-
