01a500c8890285444314768e8846f2d5bee835be
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-12 02:00:53 +0000

    removed debugging code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,53 +1,6 @@
 
 $(document).ready(function(){
 
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 function createHeader (tweet) {
   var $header = $("<header>")
       .append($("<img class='avatar' src='" + tweet.user.avatars.small + "'>"))
@@ -56,7 +9,7 @@ function createHeader (tweet) {
   return $header;
 }
 
-function createtweetBody (tweet){
+function createTweetBody (tweet){
   var $tweetBody = $("<p>").text(tweet.content.text);
   return $tweetBody;
 }
@@ -74,7 +27,7 @@ function createFooter (tweet){
 function createTweetElement (tweet) {
   var $tweet = $("<article class='tweet'>")
     .append(createHeader(tweet))
-    .append(createtweetBody(tweet))
+    .append(createTweetBody(tweet))
     .append(createFooter(tweet));
   return $tweet;
 }
@@ -85,8 +38,6 @@ function renderTweets (tweets){
     $("#tweets-container").append(tweet);
   });
 }
-
-renderTweets(data);
 
 });
 
